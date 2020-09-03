e815cc66528c9364288b5c3b11bdce94eaf58261
AechGG <hazg@me.com>
2020-09-03 20:04:18 +0100

    feat: add swift json and get full tweet

--- Twittermental/ViewController.swift
+++ Twittermental/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 import SwifteriOS
+import CoreML
+import SwiftyJSON
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var inputTextField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    let sentimentClassifier = TweetSentimentClassifier();
     
     var swifter = Swifter(consumerKey: K.TWITTER_CONSUMER_KEY, consumerSecret: K.TWITTER_CONSUMER_SECRET);
     
@@ -20,8 +24,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+//        let prediction = try! sentimentClassifier.prediction(text: "test");
+//
+//        print(prediction.label);
+        
         swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
-            print(results);
+            
+            var tweets = [String]();
+            
+            for i in 0..<100 {
+                if let tweet = results[i]["full_text"].string {
+                    tweets.append(tweet);
+                }
+            }
+            
+            
+            
         }) { (error) in
             print("Error retrieving tweets: \(error)")
         }
