5c2a55b6be1054fa37885d20fdef4d53e83a23c0
AechGG <hazg@me.com>
2020-09-03 19:30:02 +0100

    feat: add tweet model

---
+++ Twittermental/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Twittermental
+//
+//  Created by Harrison Gittos on 03/09/2020.
+//  Copyright © 2020 Harrison Gittos. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    let TWITTER_API_CONSUMER_KEY = "";
+    let TWITTER_API_CONSUMER_SECRET = "";
+}
